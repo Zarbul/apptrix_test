a8ba574063d53ef23c84d409c89ecf63aa265e11
Michael Kamenev <dlyapicem@gmail.com>
2021-10-20 01:31:22 +0300

    add parser

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('src.api.urls'))
+    path('api/', include('src.api.urls'), name='api'),
+    path('product/', include('src.products.urls'))
 ]

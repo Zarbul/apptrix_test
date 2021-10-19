f8dfb9351e7f62f8e36020c4cef431c6c5a21431
Michael Kamenev <dlyapicem@gmail.com>
2021-10-19 05:02:06 +0300

    add createuser

--- src/api/views.py
+++ src/api/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import AuthUser
-from .serializers import UserListSerializer
+from .serializers import UserListSerializer, UserCreateSerializer
 
 
 class UserListViewSet(viewsets.ModelViewSet):
     queryset = AuthUser.objects.all()
     serializer_class = UserListSerializer
+
+
+class UserCreateViewSet(viewsets.ModelViewSet):
+    # queryset = AuthUser.objects.all()
+    serializer_class = UserCreateSerializer

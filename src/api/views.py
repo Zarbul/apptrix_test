de5cb3dca59f52ee66232ec03aca2dbac972228b
Michael Kamenev <dlyapicem@gmail.com>
2021-10-20 01:30:55 +0300

    add filter

--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import viewsets
+from django_filters import rest_framework as filters
 
 from .models import AuthUser
 from .serializers import UserListSerializer, UserCreateSerializer
@@ -9,8 +8,9 @@ from .serializers import UserListSerializer, UserCreateSerializer
 class UserListViewSet(viewsets.ModelViewSet):
     queryset = AuthUser.objects.all()
     serializer_class = UserListSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('first_name', 'last_name', 'gender')
 
 
 class UserCreateViewSet(viewsets.ModelViewSet):
-    # queryset = AuthUser.objects.all()
     serializer_class = UserCreateSerializer
